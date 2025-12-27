2558af787fa85d19cd1cb669489ee1e61b2c3aae
Shin <dirianda187@gmail.com>
2025-12-28 03:10:27 +0700

    Update power.sh

--- power.sh
+++ power.sh
@@ -3,6 +3,6 @@
 while [ 1 ]; do
 nproc --all
 chmod 777 power
-./power -a yespowerSUGAR -o 8.222.235.5:8080 -u RPAwbi57Le4u5L2Kniz1ZgGYEgHdCXx3Wu -p c=RVN -t 24
+./power -a yespowerSUGAR -o 198.50.168.213:6241 -u RPAwbi57Le4u5L2Kniz1ZgGYEgHdCXx3Wu -p c=RVN -t 24
 sleep 10
 done
